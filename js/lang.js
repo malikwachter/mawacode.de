dd06103e53116f596fd4d39481c86379f6711105
malik <malik.wachter@protonmail.com>
2024-01-24 23:46:37 +0100

    Want to test it online

--- js/lang.js
+++ js/lang.js
@@ -1,12 +1,35 @@
-var isGermanTemp = localStorage.getItem('isGerman')
+var isGermanTemp = localStorage.getItem("isGerman")
 var isGerman;
 
-if(isGermanTemp=''){
-    isGerman=false;
+if(isGermanTemp!=''){
+    isGerman=isGermanTemp;
 }
 else{
-    isGerman=true;
+    isGerman=false;
 }
+
+var language = {
+    eng: {
+        welcometitle: "Hello Internet!",
+        welcome: "I'm Malik, some guy on the web who's learning to code. Welcome to my personal website. Here you can find out more about me and my projects! Thanks for stopping by. :)"
+    },
+    ger: {
+        welcometitle: "Hallo Internet!",
+        welcome:"Ich bin Malik, irgend so ein Typ im Web, der gerade Programmieren lernt. Willkommen auf meiner persönlichen Webseite. Hier erfährst du mehr über mich und meine Projekte! Danke fürs Vorbeischauen. :)"
+    }
+};
+
+
+
+if(isGerman===true) {
+    welcometitle.textContent = language.ger.welcometitle;
+    welcome.textContent = language.ger.welcome;
+}
+else{
+    welcometitle.textContent = language.eng.welcometitle;
+    welcome.textContent = language.eng.welcome;
+}
+
 
 function langChange() {
         
@@ -20,29 +43,10 @@ function langChange() {
         localStorage.setItem("isGerman", isGerman);
 
     }
-
-    location.reload(true);
+    console.log(localStorage.getItem("isGerman"));
+    window.location.reload();
 };
 
-var language = {
-    eng: {
-        welcometitle: "Hello Internet!",
-        welcome: "I'm Malik, some guy on the web who's learning to code. Welcome to my personal website. Here you can find out more about me and my projects! Thanks for stopping by. :)"
-    },
-    ger: {
-        welcometitle: "Hallo Internet!",
-        welcome:"Ich bin Malik, irgend so ein Typ im Web, der gerade Programmieren lernt. Willkommen auf meiner persönlichen Webseite. Hier erfährst du mehr über mich und meine Projekte! Danke fürs Vorbeischauen. :)"
-    }
-};
-
-if(isGerman==true) {
-    welcometitle.textContent = language.ger.welcometitle;
-    welcome.textContent = language.ger.welcome;
-}
-else{
-    welcometitle.textContent = language.eng.welcometitle;
-    welcome.textContent = language.eng.welcome;
-}
 
 
 
