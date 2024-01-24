a68ac4471e530013e65057f748edaef2f7b2da4d
malik <malik.wachter@protonmail.com>
2024-01-24 20:16:42 +0100

    Add Translation script (testing)

--- js/lang.js
+++ js/lang.js
@@ -1,16 +1,27 @@
+
+var dataReload = document.querySelectorAll("[data-reload]");
+
 var language = {
     eng: {
         welcometitle: "Hello Internet!",
-        welcome: "I'm Malik, some guy on the web who's learning to code. <br> Welcome to my personal website. Here you can find out more about me and my projects! Thanks for stopping by. :)"
+        welcome: "I'm Malik, some guy on the web who's learning to code. Welcome to my personal website. Here you can find out more about me and my projects! Thanks for stopping by. :)"
     },
     ger: {
         welcometitle: "Hallo Internet!",
-        welcome:"Ich bin Malik, irgend so ein Typ im Web, der gerade Programmieren lernt. <br> Willkommen auf meiner persönlichen Webseite. Hier erfährst du mehr über mich und meine Projekte! Danke fürs Vorbeischauen. :)"
+        welcome:"Ich bin Malik, irgend so ein Typ im Web, der gerade Programmieren lernt. Willkommen auf meiner persönlichen Webseite. Hier erfährst du mehr über mich und meine Projekte! Danke fürs Vorbeischauen. :)"
     }
 };
 
 if(window.location.hash) {
-    if(window.location.hash == "#ger") {
+    if(window.location.hash === "#ger") {
         welcome.textContent = language.ger.welcome;
     }
 }
+
+for (i = 0; i <= dataReload.length; i++) {
+    dataReload[i].onclick = function() {
+        location.reload(true);
+    };
+}
+
+
