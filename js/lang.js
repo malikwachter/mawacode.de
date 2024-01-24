36a7845c881664202180e09379f54c928b998c18
malik <malik.wachter@protonmail.com>
2024-01-24 19:35:35 +0100

    Add langchange button Add (not working) js to translate later

---
+++ js/lang.js
@@ -0,0 +1,16 @@
+var language = {
+    eng: {
+        welcometitle: "Hello Internet!",
+        welcome: "I'm Malik, some guy on the web who's learning to code. <br> Welcome to my personal website. Here you can find out more about me and my projects! Thanks for stopping by. :)"
+    },
+    ger: {
+        welcometitle: "Hallo Internet!",
+        welcome:"Ich bin Malik, irgend so ein Typ im Web, der gerade Programmieren lernt. <br> Willkommen auf meiner persönlichen Webseite. Hier erfährst du mehr über mich und meine Projekte! Danke fürs Vorbeischauen. :)"
+    }
+};
+
+if(window.location.hash) {
+    if(window.location.hash == "#ger") {
+        welcome.textContent = language.ger.welcome;
+    }
+}
